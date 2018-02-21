c8b24da12c4caf6d5e6c1cb9aaea6d9325303cbd
aaronwinter <aaron.ounn@gmail.com>
2018-02-21 09:36:46 +0100

    finitefield: Add finite field arithmetic ops

--- finitefield/finitefield.go
+++ finitefield/finitefield.go
@@ -15,3 +15,26 @@ func NewFpInt(v, order *big.Int) *FpInt {
 		Order: order,
 	}
 }
+
+func (k *FpInt) Add(a, b *FpInt) *FpInt {
+	k.Value.Add(a.Value, b.Value)
+	k.Value.Mod(k.Value, a.Order)
+	return k
+}
+
+func (k *FpInt) Sub(a, b *FpInt) *FpInt {
+	k.Value.Sub(a.Value, b.Value)
+	k.Value.Mod(k.Value, a.Order)
+	return k
+}
+
+func (k *FpInt) Mul(a, b *FpInt) *FpInt {
+	k.Value.Mul(a.Value, b.Value)
+	k.Value.Mod(k.Value, a.Order)
+	return k
+}
+
+func (k *FpInt) ModInv(a *FpInt) *FpInt {
+	k.Value.ModInverse(a.Value, a.Order)
+	return k
+}
