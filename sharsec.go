61c1da41cda4f1e229be0a34fe7c45ac1cb5b1eb
aaronwinter <aaron.ounn@gmail.com>
2018-02-21 09:39:05 +0100

    sharsec: Define interface for secret sharing

---
+++ sharsec.go
@@ -0,0 +1,39 @@
+package sharsec
+
+import (
+	"math/big"
+	"sharsec/curvewrapper"
+)
+
+//   __      ___   ___ _  _ ___ _  _  ___
+//   \ \    / /_\ | _ \ \| |_ _| \| |/ __|
+//    \ \/\/ / _ \|   / .` || || .` | (_ |
+//     \_/\_/_/ \_\_|_\_|\_|___|_|\_|\___|
+//
+// DO NOT USE THIS CODE IN PRODUCTION OR GET PWND
+// NOT USING CONSTANT-TIME ARITHMETIC OPERATIONS
+// NOT AUDITED
+
+// THIS IS JUST TO MESS AROUND. NEVER LET THIS CODE HIT PRODUCTION
+
+// SSS defines the behavior of a Secret Sharing Scheme (SSS)
+
+type Key struct {
+	priv []byte
+	pub  curvewrapper.Point
+}
+
+type Share interface {
+	Encrypt(Key) Share
+	Decrypt(Key) Share
+}
+
+type SSS interface {
+	Split(*big.Int, int, []Key) []Share
+	Combine([]Share) *big.Int
+}
+
+type Polynomial interface {
+	Eval(*big.Int, *big.Int) *big.Int
+	String() string
+}
